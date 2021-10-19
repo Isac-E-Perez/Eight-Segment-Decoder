2c771fcd2996f7e90c7394a56264da65f9ec4e43
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-19 16:04:30 -0500

    Update eight_tb.vhdl

--- eight_tb.vhdl
+++ eight_tb.vhdl
@@ -20,7 +20,7 @@ signal SZ_OUT : std_logic_vector(6 downto 0);
 
 begin 
   uut: seven port map(
-                      D_IN => D_IN,
+                      D_IN => D_IN;
                       SZ_OUT => SZ_OUT  
                       );
   stim_proc: process
