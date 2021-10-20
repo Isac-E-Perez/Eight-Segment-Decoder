c6db5fb2a2b49b925b636f579f290b1a77b443aa
Isac E Perez <89553126+Isac-E-Perez@users.noreply.github.com>
2021-10-20 15:17:17 -0500

    Update eight.vhdl

--- eight.vhdl
+++ eight.vhdl
@@ -3,23 +3,22 @@ use ieee.std_logic_1164.all;
 
 entity eight is 
   port(
-      D_IN : in std_logic_vector(3 downto 0);
-      SZ_OUT : out std_logic_vector(7 downto 0)
-      );
+    D_IN : in std_logic_vector(3 downto 0);
+    SZ_OUT : out std_logic_vector(7 downto 0)
+    );
 end eight; 
     
 architecture behavior of eight is
   
 begin 
   with D_IN select
-    SZ_OUT <=   "10000000" when "0000"|"0001",
-                "01000000" when "0010"|"0011",
-                "00100000" when "0100"|"0101",
-                "00010000" when "0110"|"0111",
-                "00001000" when "1000"|"1001",
-                "00000100" when "1010"|"1011",
-                "00000010" when "1100"|"1101",
-                "00000001" when "1110"|"1111",
-                "00000000" when others;
-  
+    SZ_OUT <= "10000000" when "0000"|"0001",
+              "01000000" when "0010"|"0011",
+              "00100000" when "0100"|"0101",
+              "00010000" when "0110"|"0111",
+              "00001000" when "1000"|"1001",
+              "00000100" when "1010"|"1011",
+              "00000010" when "1100"|"1101",
+              "00000001" when "1110"|"1111",
+              "00000000" when others;  
 end behavior;
